4c8d079c77a96d05b92e840b3fd24b0eb12b965d
KealanMcD8 <kealanmcdevitt@gmail.com>
2023-05-24 00:43:37 +0100

    Connect Model relationships

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,2 +1,5 @@
 class Exercise < ApplicationRecord
+  has_and_belongs_to_many :equipments
+  has_and_belongs_to_many :muscle_groups
+  has_many :workout_sets
 end
--- app/models/workout_set.rb
+++ app/models/workout_set.rb
@@ -1,2 +1,4 @@
 class WorkoutSet < ApplicationRecord
+  belongs_to :workout
+  belongs_to :exercise
 end
