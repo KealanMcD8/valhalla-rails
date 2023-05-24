2d4cee4adb37b2259ef6268e7f3a261179d79124
KealanMcD8 <kealanmcdevitt@gmail.com>
2023-05-24 03:30:35 +0100

    Add a User controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
