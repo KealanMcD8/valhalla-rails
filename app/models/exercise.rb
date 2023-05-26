3e188566eae509729fd54def4162472ca4c482b5
KealanMcD8 <kealanmcdevitt@gmail.com>
2023-05-26 20:29:15 +0100

    Restructure database

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,21 +1,7 @@
 class Exercise < ApplicationRecord
-  has_and_belongs_to_many :equipments
-  has_and_belongs_to_many :muscle_groups, join_table: "exercises_muscle_groups"
-  serialize :secondary_muscle_groups, Array
-
-  has_many :workout_exercises, dependent: :destroy
-  has_many :workouts, through: :workout_exercises
-  has_many :workout_sets, dependent: :destroy
-
-  belongs_to :primary_muscle_group, class_name: 'MuscleGroup'
-  has_many :exercise_secondary_muscle_groups
-  has_many :secondary_muscle_groups, through: :exercise_secondary_muscle_groups, source: :muscle_group
-
   has_many :exercise_equipments, dependent: :destroy
-  has_many :equipment_exercises, through: :exercise_equipments, source: :equipment
-
-  accepts_nested_attributes_for :exercise_equipments
-
-  # Add the following line to include the primary_muscle_group_id attribute
-  attribute :primary_muscle_group_id, :integer
+  has_many :equipments, through: :exercise_equipments
+  belongs_to :primary_muscle_group, class_name: 'MuscleGroup'
+  has_and_belongs_to_many :secondary_muscle_groups, class_name: 'MuscleGroup'
+  has_many :workout_sets, dependent: :destroy
 end
