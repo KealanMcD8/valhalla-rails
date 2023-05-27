04bc19f8e639d15e0ef97df0fde30d507eebb2f3
KealanMcD8 <kealanmcdevitt@gmail.com>
2023-05-27 04:07:49 +0100

    Add create Equipment

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
     field :createMuscleGroup, mutation: Mutations::CreateMuscleGroup
+    field :createEquipment, mutation: Mutations::CreateEquipment
   end
 end
